458525689931ceb43065d11b4865e4947a93942f
Anuchit Chalothorn <anoochit@gmail.com>
2014-09-09 23:31:25 +0700

    fix timestamp

--- dht22.py
+++ dht22.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import time
+import datetime
 import Adafruit_DHT
 from time import gmtime, strftime
  
@@ -11,8 +12,9 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
  
 while True:
   if humidity is not None and temperature is not None:
-    ctime=strftime("%a, %d %b %Y %X +0000", gmtime())
-    print 'Date={0} Temp={1:0.1f}*C  Humidity={2:0.1f}%'.format(ctime,temperature, humidity)
+    #ctime=strftime("%a, %d %b %Y %X +0000", gmtime())
+    ctime=datetime.datetime.fromtimestamp(time.time(), None)
+    print 'Date={0} Temp={1:0.2f}*C  Humidity={2:0.2f}%'.format(ctime,temperature, humidity)
     time.sleep(5)
   else:
     print 'Failed to get reading. Try again!'
